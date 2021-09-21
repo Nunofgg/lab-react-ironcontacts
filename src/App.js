f28bda2c340f2fc8233f16ec6c36b60fd0a8132b
Nunofgg <goncalvesnuno00@gmail.com>
2021-09-21 15:01:14 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
+import contactsJSON from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = useState(contactsJSON.slice(0, 5));
+
+  const addRandomContact = () =>
+    setContacts([
+      contactsJSON[Math.floor(Math.random() * contactsJSON.length)],
+      ...contacts
+    ]);
+
+  const sortFunction = (typeOfSort) => {
+    setContacts([...contacts.sort((contact1, contact2) => {
+      return contact1[typeOfSort] > contact2[typeOfSort] ? 1 : -1;
+    })]);
+  }
+
+  const deleteContact = (id) => setContacts(contacts.filter((contact) => contact.id !== id));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={addRandomContact}>Add random contact</button>
+      <button onClick={() => sortFunction("name")}>Sort by name</button>
+      <button onClick={() => sortFunction("popularity")}>Sort by popularity</button>
+      <h1>IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <div key={contact.id}>
+                <tr>
+                  <td>
+                    <img src={contact.pictureUrl} width="200" />
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity.toFixed([2])}</td>
+                  <td>{contact.wonOscar ? "🏆" : ""}</td>
+                  <td>{contact.wonEmmy ? "🏆" : ""}</td>
+                </tr>
+                <button onClick={() => deleteContact(contact.id)}>Delete contact</button>
+              </div>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/List.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+
+function List() {
+    return (
+        <table>
+            <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+            </tr>
+        </table>
+    )
+}
+
+export default List;
